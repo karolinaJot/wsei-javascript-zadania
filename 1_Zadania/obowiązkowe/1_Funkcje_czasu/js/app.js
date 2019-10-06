8642ae1d0e744f1dc6c2f852341fbb516bd1079b
Jaklik Karolina <karolina.jaklik@microsoft.wsei.edu.pl>
2019-10-06 15:07:01 +0200

    rozwiazanie zad1

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,14 @@
+//zadanie1
+
+function countHello(number){
+    var counter = 1;
+    var interval = setInterval(() =>{
+        if(counter == number){
+            clearInterval(interval)
+        }
+        console.log("Hello " , counter);
+        counter++;
+    }, 500)
+}
+
+countHello(5);

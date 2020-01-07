65c3b991ecd0cfb845667104ec6ec2e47b87e830
karolinaJot <amicus.karo@gmail.com>
2020-01-07 11:38:15 +0100

    5_this - done

--- 1_Zadania/obowiązkowe/5_this/js/app.js
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -1,6 +1,5 @@
 // zadanie 0
-
-const car = {
+let car = {
     brand: 'Fiat',
     color: 'Czerwony',
     numberOfKilometers: 0,
@@ -13,5 +12,35 @@ const car = {
 }
 
 car.printCarInfo();
-car.numberOfKilometers(20);
+car.drive(20);
 car.printCarInfo();
+
+// zadanie 1
+car.service = ['02.04.1999', '02.04.2000', '02.04.2001'];
+car.addService = function(date){
+    this.service.push(date);
+}
+car.printAllServices = function(){
+    for(let i = 0; i < this.service.length; i++){
+        console.log(this.service[i]);
+    }
+}
+
+car.addService('02.04.2002');
+car.printAllServices();
+
+
+// zadanie 3
+let stairs = {
+    step: 0,
+    up: function(){
+        this.step++;
+    },
+    down: function(){
+        this.step--;
+    },
+    printStep: function(){
+        console.log(this.step);
+    }
+}
+
--- 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
@@ -3,9 +3,20 @@ var calculator = {
         this.a = newA;
         this.b = newB;
     }
-
 };
+
+calculator.sum = function(){
+    let result = 0;
+    return result = this.a + this.b;
+}
+calculator.multiply = function(){
+    let result = 0;
+    return result = this.a * this.b;
+}
 
 calculator.save(2, 3);
 console.log(calculator.sum());
 console.log(calculator.multiply());
+
+
+

786cff6a615f699b2ca1fa8ac294df2a94054769
karolinaJot <amicus.karo@gmail.com>
2020-02-03 21:17:30 +0100

    Menu

---
+++ 1_Zadania/dodatkowe/4_Menu/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded and parsed!');
+
+    let listItems = document.querySelector('.nav ul').children;
+    console.log(listItems);
+
+    for (let i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener('mouseover', function (e) {
+            // console.log('mouseover');
+            if (this.children.length > 0) {
+                this.children[0].style.display = 'block';
+            }
+        });
+
+        listItems[i].addEventListener('mouseout', function (e) {
+            // console.log('mouseout');
+            if (this.children.length > 0) {
+                this.children[0].style.display = 'none';
+            }
+        });
+
+    }
+
+});

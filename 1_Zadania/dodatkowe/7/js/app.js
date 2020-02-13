9fe0b1aed30ccb3d578ddb8df8d138a881ddd11c
karolinaJot <amicus.karo@gmail.com>
2020-02-13 13:00:34 +0100

    additional tasks from 7 - all

--- 1_Zadania/dodatkowe/7/js/app.js
+++ 1_Zadania/dodatkowe/7/js/app.js
@@ -38,3 +38,78 @@ function createIdentityMatrix(rows, columns) {
 }
 
 console.log(createIdentityMatrix(4, 4));
+
+// zadanie 1 -dodatkowe
+function addTheSameNumbers(number, array) {
+    let result = null;
+    for (let i = 0; i < array.length; i++) {
+        if (number === array[i]) {
+            result += array[i]
+        }
+    }
+    return result;
+}
+
+console.log(addTheSameNumbers(7, [4, 7, 24, 7, 0, 10]));
+console.log(addTheSameNumbers(9, [33, 54, 2, 1, 9, 100]));
+console.log(addTheSameNumbers(0, [33, 0, 2, 0, 9, 0]));
+console.log(addTheSameNumbers(43, [6, 0, 2, 2, 3, 46]));
+
+// zadanie 2 -dodatkowe
+function factors(number) {
+    const result = [];
+    if (number <= 0) {
+        result;
+    } else {
+        let factor = number;
+        for (let i = 1; i <= number; i++) {
+            if (number % factor === 0) {
+                result.push(factor);
+            }
+            factor--;
+        }
+    }
+    return result;
+}
+
+console.log(factors(-4));
+
+// zadanie 3 -dodatkowe
+function getMissingElement(array) {
+    let missingElement = null;
+    let index = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === index) {
+            index++;
+        } else {
+            missingElement = index;
+        }
+    }
+    return missingElement;
+}
+console.log(getMissingElement([1, 2, 3, 4, 5, 6, 7]));
+console.log(getMissingElement([6, 7, 8, 10, 11, 12, 13, 14, 15]));
+console.log(getMissingElement([-4, -3, -2, 0, 1, 2, 3, 4]));
+
+
+// zadanie 4 - dodatkowe
+function getLastNumbers(number, array) {
+    const lastNumbers = [];
+
+    if (number === 'undefined' || typeof (number) !== 'number') {
+        return lastNumbers;
+    } else {
+        let index = array.length - number;
+        for (let i = 0; i < array.length; i++) {
+            if (i === index) {
+                lastNumbers.push(array[i]);
+                index++
+            }
+        }
+    }
+    return lastNumbers;
+}
+console.log(getLastNumbers(2, [1, 2, 3, 4, 5, 6, 7]));
+console.log(getLastNumbers(4, [6, 7, 8, 10, 11, 12, 13, 14, 15]));
+console.log(getLastNumbers([-4, -3, -2, 0, 1, 2, 3, 4]));
+console.log(getLastNumbers('ala', [-4,-3,-2,0,1,2,3,4]));
